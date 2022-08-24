179c5f5d5a88d781334900671ac19dbff36fb737
nmaresco <109701914+nmaresco@users.noreply.github.com>
2022-08-24 14:27:53 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let pokemonList = [
+  {name:'Pikachu',height:4,type:'electric'},
+  {name:'Charmander',height:6,type:'fire'},
+  {name:'Squirtle', height:5,type:'water'}
+];
+
+for (let i = 0; i <=2;i++) {
+      document.write(pokemonList[i].name),
+      document.write(pokemonList[i].height),
+      document.write(pokemonList[i].type)
+};
