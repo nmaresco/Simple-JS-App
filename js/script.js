7c4b40426f6452a7cc17c1a1efd7a67584e5a841
nmaresco <109701914+nmaresco@users.noreply.github.com>
2022-08-27 14:48:37 -0700

    Updated JS to be more stable.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-let pokemonList = [
-  {name:'Pikachu',height:4,type:'electric'},
-  {name:'Charmander',height:6,type:'fire'},
-  {name:'Squirtle', height:5,type:'water'}
-];
-
-pokemonList.forEach(writetoDocument());
-
-
 function writetoDocument() {
+  let pokemonList = [
+    {name:'Pikachu',height:4,type:'electric'},
+    {name:'Charmander',height:6,type:'fire'},
+    {name:'Squirtle', height:5,type:'water'}
+  ];
   for (var i = 0; i < pokemonList.length; i++) {
   document.write(pokemonList[i].name + '<br/>');
   document.write(pokemonList[i].height + '<br/>');
@@ -15,3 +11,17 @@ function writetoDocument() {
   document.write('<br/>');
   }
 }
+function getAll (){
+  let pokemonList = [
+    {name:'Pikachu',height:4,type:'electric'},
+    {name:'Charmander',height:6,type:'fire'},
+    {name:'Squirtle', height:5,type:'water'}
+  ];
+  let pokemonRepository = pokemonList;
+};
+function add(item){
+  pokemonList = [item];
+}
+writetoDocument();
+getAll();
+add();
