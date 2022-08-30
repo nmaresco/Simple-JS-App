8214ffffd31b5483c410e7621be5f53a92195805
nmaresco <109701914+nmaresco@users.noreply.github.com>
2022-08-29 19:11:50 -0700

    Replaced JS code with correct code
    Received tutoring help.
    Was given code example.
    Used it in place of mine after going over the task with tutor.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
-function writetoDocument() {
-  let pokemonList = [
-    {name:'Pikachu',height:4,type:'electric'},
-    {name:'Charmander',height:6,type:'fire'},
-    {name:'Squirtle', height:5,type:'water'}
-  ];
-  for (var i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + '<br/>');
-  document.write(pokemonList[i].height + '<br/>');
-  document.write(pokemonList[i].type + '<br/>');
-  document.write('<br/>');
+///IIFE
+const pokemonRepository = (function(){
+
+  const pokemonList = []
+  // const pokemonList = [
+  //   {name:'Pikachu',height:4,type:'electric'},
+  //   {name:'Charmander',height:6,type:'fire'},
+  //   {name:'Squirtle', height:5,type:'water'}
+  // ];
+
+  function getAll (){
+    return pokemonList;
+  };
+
+  function add(item){
+    //ensure data type match
+    if(typeof item.name != "string" && typeof item.height != "number" && typeof type != "string") {
+      return
+    }
+
+    //remove duplicates from list
+    const foundItem = pokemonList.find(function(pokemon) {
+      return (pokemon.name === item.name && pokemon.height === item.height && pokemon.type === item.type)
+    })
+
+    if(!foundItem) {
+      pokemonList.push(item);
+    }
   }
-}
-function getAll (){
-  let pokemonList = [
-    {name:'Pikachu',height:4,type:'electric'},
-    {name:'Charmander',height:6,type:'fire'},
-    {name:'Squirtle', height:5,type:'water'}
-  ];
-  let pokemonRepository = pokemonList;
-};
-function add(item){
-  pokemonList = [item];
-}
-writetoDocument();
-getAll();
-add();
+  //SHOULD ONLY RETURN WHAT IS NEEDED OUTSIDE THE FUNCTION
+  return {
+    getAll,
+    add,
+  }
+})()
+
+//Uncomment this to test the add function
+const pokemonListToAdd =  [
+  {name:'Pikachu',height:4,type:'electric'},
+  {name:'Charmander',height:6,type:'fire'},
+  {name:'Squirtle', height:5,type:'water'},
+  {name:'Squirtle', height:5,type:'water'}
+];
+
+pokemonListToAdd.forEach(function(pokemon) {
+  pokemonRepository.add(pokemon)
+})
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name + '<br/>');
+  document.write(pokemon.height + '<br/>');
+  document.write(pokemon.type + '<br/>');
+  document.write('<br/>');
+})
